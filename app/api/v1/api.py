ae32870a33c0ccb80f81c50f1ce7ad7991ad2886
Chalaka-Ccb <COHNDSE242F-074@student.nibm.lk>
2025-10-25 08:32:23 +0530

    Set User profile management

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import trips
+from app.api.v1.endpoints import users
 
 api_router = APIRouter()
 api_router.include_router(trips.router, prefix="/trips", tags=["Trips & Planning"])
+
+# <-- 2. ADD THE NEW USER ROUTER -->
+api_router.include_router(users.router, prefix="/users", tags=["User Profile"])
